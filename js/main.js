358264f7c5446c23159e84ee70fb7f71b5a6c356
Luka <luka.luka.luka93@gmail.com>
2024-01-12 15:27:41 -0800

    HTML and CSS done, JS in progress

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Button to claim your free trial
+const button = document.querySelector("button");
+
+// All inputs
+const allInput = document.querySelectorAll("input");
+
+// Input area for first name
+const inputName = document.querySelector("#name");
+
+// Input area for the Last Name
+const inputLastName = document.querySelector("#last-name");
+
+// Input area for your email address
+const email = document.querySelector("#email");
+
+// Input area for the password
+const password = document.querySelector("#password");
+
+// Input container
+const inputContainer = document.querySelector(".input-container");
+
+
+// ERROR
+const errorLogo = document.querySelectorAll(".error-logo");
+const errorText = document.querySelectorAll(".error-text");
+
+const errorLogoName = document.querySelector("#name-error");
+const errorName = document.querySelector("#text-error-name");
+
+
+
+
